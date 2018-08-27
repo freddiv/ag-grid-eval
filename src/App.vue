f1e8a1f4abeef15465299f2ec6f97b479fb51c0b
Valone <valone.carlf@epa.gov>
2018-08-27 14:57:36 -0400

    First commit of ag-grid evaluation project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+    <CarGrid />
+    <ChemGrid /> 
+    </div>  
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CarGrid from './components/CarGrid.vue'
+import ChemGrid from './components/ChemGrid.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CarGrid,
+    ChemGrid
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from "vue-router";
+import "../node_modules/ag-grid/dist/styles/ag-grid.css";
+import "../node_modules/ag-grid/dist/styles/ag-theme-balham.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import 'ag-grid-enterprise';
 
+
+Vue.use(VueRouter);
 Vue.config.productionTip = false
 
 new Vue({
